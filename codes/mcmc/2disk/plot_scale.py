99f515d10b60ff5a4b81c95f68d487c94a53ed95
aszewciw <aszewci@gmail.com>
2015-10-16 16:21:02 -0500

    changed plotting

--- codes/mcmc/2disk/plot_scale.py
+++ codes/mcmc/2disk/plot_scale.py
@@ -7,10 +7,10 @@ import matplotlib
 Read files of correlation data and plot them, including errors in mean
 '''
 
-z_n = np.genfromtxt('Z_THN.dat')
-z_k = np.genfromtxt('Z_THC.dat')
-r_n = np.genfromtxt('R_THN.dat')
-r_k = np.genfromtxt('R_THC.dat')
+z_n = np.genfromtxt('Z_THIN.dat')
+z_k = np.genfromtxt('Z_THICK.dat')
+r_n = np.genfromtxt('R_THIN.dat')
+r_k = np.genfromtxt('R_THICK.dat')
 a = np.genfromtxt('A.dat')
 
 print(a)
