f4ac217758498bce748b8ca9fdb79479b7c92dda
Adam Szewciw <aszewci@gmail.com>
2016-02-12 14:32:25 -0600

    compatibility

--- codes/mcmc/2disk/plot_scale.py
+++ codes/mcmc/2disk/plot_scale.py
@@ -7,15 +7,17 @@ import matplotlib
 Read files of correlation data and plot them, including errors in mean
 '''
 
-z_n = np.genfromtxt('Z_THIN.dat')
-z_k = np.genfromtxt('Z_THICK.dat')
-r_n = np.genfromtxt('R_THIN.dat')
-r_k = np.genfromtxt('R_THICK.dat')
-a = np.genfromtxt('A.dat')
+z_n = np.genfromtxt('Z_THIN1.dat')
+z_k = np.genfromtxt('Z_THICK1.dat')
+r_n = np.genfromtxt('R_THIN1.dat')
+r_k = np.genfromtxt('R_THICK1.dat')
+a = np.genfromtxt('A1.dat')
 
-print(a)
+index = np.arange(0, len(a))
 
 plt.figure(1)
+#plt.plot(index, z_n, 'ko', index, z_k, 'bo', index, r_n, 'ro', index, r_k, 'go')
+#plt.show()
 plt.subplot(211)
 plt.xlabel(r'$\ R_{0, thin}$ (kpc)')
 plt.ylabel(r'$\ R_{0, thick}$ (kpc)')
@@ -25,4 +27,6 @@ plt.subplot(212)
 plt.xlabel(r'$\ Z_{0, thin}$ (kpc)')
 plt.ylabel(r'$\ Z_{0, thick}$ (kpc)')
 plt.plot(z_n, z_k, 'ko')
+
 plt.show()
+#plt.savefig('5sig.png')
