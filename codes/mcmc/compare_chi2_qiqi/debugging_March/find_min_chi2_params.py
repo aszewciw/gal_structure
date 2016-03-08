b481f739224d099bb4287c1b59862a5697a8142e
aszewciw <aszewci@gmail.com>
2016-03-08 11:36:45 -0600

    Added input filename and output lines for storing parameters.

--- codes/mcmc/compare_chi2_qiqi/debugging_March/find_min_chi2_params.py
+++ codes/mcmc/compare_chi2_qiqi/debugging_March/find_min_chi2_params.py
@@ -4,15 +4,36 @@ from config import *
 
 #find parameters for Qingqing's minimum chi2 value
 
-qiqi_file = qiqi_dir + 'mcmc_result.dat'
+def main():
+    elements_needed = int(2)
 
-rthin, zthin, rthick, zthick, a, chi2 = np.genfromtxt(qiqi_file, unpack=True, usecols=[1, 2, 3, 4, 5, 6])
+    args_array = np.array(sys.argv)
+    N_args = len(args_array)
+    assert(N_args == elements_needed)
+    in_file = args_array[1]
 
-min_index = np.argmin(chi2)
+    raw_path = '/fs1/mqq/Projects/StarClustering/'
 
-print('Best chi2 is:   ', chi2[min_index])
-print('Best rthin is:  ', rthin[min_index])
-print('Best zthin is:  ', zthin[min_index])
-print('Best rthick is: ', rthick[min_index])
-print('Best zthick is: ', zthick[min_index])
-print('Best a is:      ', a[min_index])
+    qiqi_file = raw_path + in_file
+
+    # qiqi_file = qiqi_dir + 'mcmc_result.dat'
+
+    rthin, zthin, rthick, zthick, a, chi2 = np.genfromtxt(qiqi_file,
+        unpack=True, usecols=[1, 2, 3, 4, 5, 6])
+
+    min_index = np.argmin(chi2)
+
+    print('Best chi2 is:   ', chi2[min_index])
+    print('Best rthin is:  ', rthin[min_index])
+    print('Best zthin is:  ', zthin[min_index])
+    print('Best rthick is: ', rthick[min_index])
+    print('Best zthick is: ', zthick[min_index])
+    print('Best a is:      ', a[min_index])
+
+    result_file = './data/' + in_file
+    result_data = np.array([chi2[min_index], rthin[min_index], zthin[min_index],
+        rthick[min_index], zthick[min_index], a[min_index]])
+    np.savetxt(result_file, result_data)
+
+if __name__ == '__main__':
+    main()
