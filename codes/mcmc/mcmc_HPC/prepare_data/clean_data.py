1d3717c78484a6ea61bbc629626acacff03edf16
aszewciw <aszewci@gmail.com>
2016-04-16 16:05:03 -0500

    changed loading line

--- codes/mcmc/mcmc_HPC/prepare_data/clean_data.py
+++ codes/mcmc/mcmc_HPC/prepare_data/clean_data.py
@@ -27,25 +27,24 @@ def main():
     err_dir   = data_dir + 'errors/'
     model_dir = data_dir + 'model_positions/'
 
-    input_filename = raw_dir + 'todo_list.dat'
-    input_file     = open(input_filename, 'rb')
-    todo_list      = pickle.load(input_file)
-    input_file.close()
+    todo_file = raw_dir + 'todo_list.ascii.dat'
+    ID        = np.genfromtxt(todo_file, unpack=True, usecols=[0], dtype=str)
 
-    for p in todo_list:
+
+    for p in ID:
 
         # Repack file containing normalized DD only
-        dd_file = dd_dir + 'DD_' + p.ID + '.dat'
+        dd_file = dd_dir + 'DD_' + p + '.dat'
         dd      = np.genfromtxt(dd_file, usecols=[2])
-        outfile = dd_dir + 'dd_' + p.ID + '.dat'
+        outfile = dd_dir + 'dd_' + p + '.dat'
         np.savetxt(outfile, dd)
 
         # Repack file containing Z, R, and W=1.0 only
-        ZRW_file = model_dir + 'uniform_' + p.ID + '.ascii.dat'
+        ZRW_file = model_dir + 'uniform_' + p + '.ascii.dat'
         Z, R, W  = np.genfromtxt(ZRW_file, unpack=True, skip_header=1,
             usecols=[5, 6, 10], dtype=None)
         N_points = len(Z)
-        outfile  = model_dir + 'zrw_' + p.ID + '.dat'
+        outfile  = model_dir + 'zrw_' + p + '.dat'
         with open(outfile, 'w') as f:
             f.write(str(N_points))
             f.write('\n')
@@ -54,15 +53,15 @@ def main():
                     str(Z[i]), str(R[i]), str(W[i])))
 
         # Repack uniform error files
-        uni_jk_file = err_dir + 'uniform_' + p.ID + '_jk_error.dat'
+        uni_jk_file = err_dir + 'uniform_' + p + '_jk_error.dat'
         uni_jk_err  = np.genfromtxt(uni_jk_file, unpack=True, usecols=[7])
-        outfile     = err_dir + 'uniform_' + p.ID + '_frac_error.dat'
+        outfile     = err_dir + 'uniform_' + p + '_frac_error.dat'
         np.savetxt(outfile, uni_jk_err)
 
         # Repack data error files
-        dat_jk_file = err_dir + 'star_' + p.ID + '_jk_error.dat'
+        dat_jk_file = err_dir + 'star_' + p + '_jk_error.dat'
         dat_jk_err  = np.genfromtxt(dat_jk_file, unpack=True, usecols=[7])
-        outfile     = err_dir + 'star_' + p.ID + '_jk_error.dat'
+        outfile     = err_dir + 'star_' + p + '_jk_error.dat'
         np.savetxt(outfile, dat_jk_err)
 
 if __name__ == '__main__':
