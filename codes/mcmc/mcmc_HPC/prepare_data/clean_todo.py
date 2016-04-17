ddde685fb5c51bda7d5c27b4f500c821199308a8
aszewciw <aszewci@gmail.com>
2016-04-16 19:14:54 -0500

    str to int

--- codes/mcmc/mcmc_HPC/prepare_data/clean_todo.py
+++ codes/mcmc/mcmc_HPC/prepare_data/clean_todo.py
@@ -6,7 +6,7 @@ import numpy as np
 def main():
 
     todo_file = '../data/raw/todo_list.ascii.dat'
-    ID        = np.genfromtxt(todo_file, unpack=True, usecols=[0], dtype=str)
+    ID        = np.genfromtxt(todo_file, unpack=True, usecols=[0], dtype=int)
     outfile   = '../data/raw/pointing_ID.dat'
     np.savetxt(outfile, ID)
 
