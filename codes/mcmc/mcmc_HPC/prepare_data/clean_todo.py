dd35ad6f9205795e9d9a541edf400d643c129456
aszewciw <aszewci@gmail.com>
2016-04-16 19:12:28 -0500

    bleh

---
+++ codes/mcmc/mcmc_HPC/prepare_data/clean_todo.py
@@ -0,0 +1,14 @@
+'''
+More dumb stuff: make todo have only pointing IDs
+'''
+import numpy as np
+
+def main():
+
+    todo_file = '../data/raw/todo_list.ascii.dat'
+    ID        = np.genfromtxt(todo_file, unpack=True, usecols=[0], dtype=str)
+    outfile   = '../data/raw/pointing_ID.dat'
+    np.savetxt(outfile, ID)
+
+if __name__ == '__main__':
+    main()
