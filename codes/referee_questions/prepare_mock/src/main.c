35aa606cb553fffd5da2203d7f8b012ffea93442
aszewciw <aszewci@gmail.com>
2016-05-17 15:07:26 -0500

    maybe?

--- codes/referee_questions/prepare_mock/src/main.c
+++ codes/referee_questions/prepare_mock/src/main.c
@@ -63,7 +63,9 @@ int main( int argc, char **argv ){
             fprintf(stderr, "%d pointings need more stars.\n", pointings_in_need);
             fprintf(stderr, "Making more stars. \n")
         }
-        else fprintf(stderr, "All poitings have an adequate number of stars. \n");
+        else{
+            fprintf(stderr, "All poitings have an adequate number of stars. \n");
+        }
 
     }
 
