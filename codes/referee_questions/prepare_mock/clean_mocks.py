24e2ca7ca74b42d1643a492ff11b4e36d0eb9634
aszewciw <aszewci@gmail.com>
2016-05-17 14:54:15 -0500

    less stars per iteration, fixing python file

--- codes/referee_questions/prepare_mock/clean_mocks.py
+++ codes/referee_questions/prepare_mock/clean_mocks.py
@@ -48,18 +48,21 @@ def main():
         N_mock = len(xyz)
         diff = N_mock - N_data
         if diff < 0:
-            print("Oh no! We didn't make enough stars!")
+            print("Oh no! We didn't make enough stars for " ID_current)
             continue
         delete_me = np.arange(diff)
         np.random.shuffle(xyz)
         xyz = np.delete(xyz, delete_me, 0)
+        if N_data != len(xyz):
+            print("Something went wrong! Incorrect number of stars for " + ID_current)
+            continue
 
         # Output new data
         out_file = OUT_DIR + 'mock_' + ID_current + '.xyz.dat'
         np.savetxt(out_file, xyz, fmt='%1.6f')
 
         # Add number of elements as first line in file
-        line_prepender(out_file, str(int(N_mock)))
+        line_prepender(out_file, str(int(N_data)))
 
     print('Data cleaned. Mocks completed.\n')
 
--- codes/referee_questions/prepare_mock/run.sh
+++ codes/referee_questions/prepare_mock/run.sh
@@ -6,7 +6,7 @@ rm ./data/temp*
 make cleanall
 make
 
-time ./bin/make_galaxy 10000000
+time ./bin/make_galaxy 1000000
 python clean_mocks.py
 
 rm ./data/temp*
