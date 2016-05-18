84dc7d7b6187db767db943da15d2d7ef90139462
aszewciw <aszewci@gmail.com>
2016-05-18 10:32:43 -0500

    bash script mod and prepare segue data

--- codes/referee_questions/prepare_segue_data/data_prepare.sh
+++ codes/referee_questions/prepare_segue_data/data_prepare.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# python pointing_list.py
+python pointing_list.py
 
 rm -f ../data/gstar_*.dat
 
 python pickle_gstar_sample.py
 
 python separate_sample.py
+
+rm -f ./data/star*
+
+python prepare_segue_data.py
---
+++ codes/referee_questions/prepare_segue_data/prepare_segue_data.py
@@ -0,0 +1,131 @@
+
+from config import *
+
+'''
+Reads in list of g-stars, does some selection, then outputs:
+
+( one of these )
+1. pickled todo_list
+2. ascii todo_list
+
+( one of these for each pointing )
+3. pickled star list (do I use this anywhere?)
+4. xyzw ascii file for correlation function
+5. ascii file containing all coordinates (used in mcmc/jackknife)
+'''
+
+#------------------------------------------------------------------------------
+def main():
+
+    # load pointing list
+    input_filename = rawdata_dir + 'pointing_list.dat'
+    sys.stderr.write('Loading from file {} ...\n'.format(input_filename))
+    input_file     = open(input_filename, 'rb')
+    pointing_list  = pickle.load(input_file)
+    input_file.close()
+
+    sys.stderr.write('Prepare files for correlation function calculation..\n')
+
+    # a list store the pointings to be used in calculation
+    todo_list = []
+
+    for p in pointing_list:
+
+        star_filename = rawdata_dir + 'gstar_' + p.ID + '.dat'
+        # skip empty files
+        if not os.path.isfile(star_filename):
+            continue
+        if os.path.getsize(star_filename) == 0:
+            continue
+
+        star_file = open(star_filename, 'rb')
+        star_list = pickle.load(star_file)
+
+        # calculate the weights
+        for s in star_list:
+            s.weight = s.r_mag_weight_sn10 * s.type_weight_sn10
+
+        # only include star with non-zero weight
+        star_list = [s for s in star_list if s.weight != 0]
+
+        # do some selection
+        if len(star_list) < 50:
+            continue # skip pointing with very few stars
+
+        # check if the stars are in range
+        r1    = INNER_DISTANCE_LIMIT
+        r2    = OUTER_DISTANCE_LIMIT
+        rlist = [s.distance for s in star_list]
+        if min(rlist) > r1 or max(rlist) < r2:
+            continue # skip pointing with stars only in too narrow range
+
+        # only use stars within the range
+        star_list = [s for s in star_list if s.distance > r1 and s.distance < r2]
+
+        # add the number of stars to the pointing object
+        p.N_star = len(star_list)
+        # store it into the todo list
+        todo_list.append(p)
+
+        # output a repacked data file
+        output_filename = data_dir + 'star_' + p.ID + '.dat'
+        output_file     = open(output_filename, "wb")
+        pickle.dump(star_list, output_file)
+        output_file.close()
+
+        # output ascii file for correlation function calculation
+        output_filename = data_dir + 'star_' + p.ID + '.xyzw.dat'
+        output_file     = open(output_filename, 'w')
+        output_file.write('{}\n'.format(len(star_list)))
+        for s in star_list:
+            output_file.write('{}\t{}\t{}\t{}\n'
+                              .format(s.cartesian_x, s.cartesian_y, s.cartesian_z,
+                                      s.weight)
+                              )
+        output_file.close()
+
+        # output ascii file for correlation function calculation
+        output_filename = data_dir + 'star_' + p.ID + '.ascii.dat'
+        output_file     = open(output_filename, 'w')
+        # first output the total number of points
+        output_file.write('{}\n'.format(len(star_list)))
+        for i in star_list:
+            output_file.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'
+                              .format(i.ra_rad, i.dec_rad, i.distance,
+                                      i.galactic_l_rad, i.galactic_b_rad,
+                                      i.galactic_Z, i.galactic_R,
+                                      i.cartesian_x, i.cartesian_y, i.cartesian_z,
+                                      i.weight))
+        output_file.close()
+
+
+    # output the todo list
+    output_filename = rawdata_dir + 'todo_list.dat'
+    output_file     = open(output_filename, 'wb')
+    pickle.dump(todo_list, output_file)
+    sys.stderr.write('The todo list is output to {} .\n'.format(output_filename))
+
+    # output todo_list as ascii file
+    output_filename = rawdata_dir + 'todo_list.ascii.dat'
+    output_file     = open(output_filename, "w")
+
+    # first output total number of pointings
+    output_file.write('{}\n'.format(len(todo_list)))
+
+    for p in todo_list:
+        output_file.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'
+                          .format(p.ID, p.ra_deg, p.dec_deg, p.ra_rad, p.dec_rad,
+                                  p.galactic_l_rad, p.galactic_b_rad,
+                                  p.cartesian_x, p.cartesian_y, p.cartesian_z,
+                                  p.N_star))
+
+    output_file.close()
+
+
+if __name__ == '__main__' :
+    main()
+
+
+
+
+
