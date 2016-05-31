1dcf0620615527e40d6571135649c78cbfb16389
aszewciw <aszewci@gmail.com>
2016-05-31 14:39:11 -0500

    jofdjifjdoj

--- codes/referee_questions/mcmc_1000_mocks/prepare_mcmc/config.py
+++ codes/referee_questions/mcmc_1000_mocks/prepare_mcmc/config.py
@@ -34,7 +34,7 @@ mcmc_out_dir = data_dir + 'mcmc_output/'
 mock_dd_dir  = data_dir + 'mock_dd/'
 pairs_dir    = data_dir + 'model_pairs/'
 zrw_dir      = data_dir + 'model_positions/'
-bins_dir     = data_dir + 'rbins/'
+rbins_dir    = data_dir + 'rbins/'
 sigma_dir    = data_dir + 'mean_var_std/'
 
 R_min = 0.005
