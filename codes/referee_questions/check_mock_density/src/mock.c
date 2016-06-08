f0ade85a36c7ab080869e47e13eb338ec70b426c
aszewciw <aszewci@gmail.com>
2016-06-08 10:38:13 -0500

    checking bisection

--- codes/referee_questions/check_mock_density/src/mock.c
+++ codes/referee_questions/check_mock_density/src/mock.c
@@ -74,7 +74,7 @@ double random_gal_R(double r0, double pdf_norm, double r_min, double r_max)
         else a = c;
     }
 
-    if(i>max_steps) fprintf(stderr, "Oh no! Took more than 100 steps to converge!\n");
+    if(i==max_steps) fprintf(stderr, "Oh no! Took more than 100 steps to converge!\n");
 
     return c;
 }
