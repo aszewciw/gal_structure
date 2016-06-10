56536add5338a96d22cab1bfa46fc43c8f776f48
aszewciw <aszewci@gmail.com>
2016-06-10 00:38:25 -0500

    jk

---
+++ codes/referee_questions/test_alt_dist/mcmc_mock/prepare_jackknife/run_jk_uniform.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+rm ../data/rbins/*.dat
+python set_rbins.py
+
+rm ../data/jackknife/uniform*
+rm counts
+
+icc -Wall -xHost -O3 -vec_report2 counts.c -o counts
+
+python prepare_uniform_jackknife.py
+python uniform_error_jk.py
