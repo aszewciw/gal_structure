e7a619f6344f1e579a79714ba32a36934840b1a4
aszewciw <aszewci@gmail.com>
2016-06-16 19:30:34 -0500

    weird

--- codes/referee_questions/mcmc_mock/prepare_jackknife/run_jk_both.sh
+++ codes/referee_questions/mcmc_mock/prepare_jackknife/run_jk_both.sh
@@ -2,14 +2,3 @@
 
 bash run_jk_mock.sh
 bash run_jk_uniform.sh
-
-rm ../data/rbins/*.dat
-python set_rbins.py
-
-rm ../data/jackknife/mock*
-rm counts
-
-icc -Wall -xHost -O3 -vec_report2 counts.c -o counts
-
-python prepare_mock_jackknife.py
-python mock_error_jk.py
