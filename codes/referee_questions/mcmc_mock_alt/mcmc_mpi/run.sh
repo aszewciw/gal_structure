b8b61cefb665c816009f02094fb62bfbf4e48ed3
aszewciw <aszewci@gmail.com>
2016-06-16 19:19:25 -0500

    running chain

--- codes/referee_questions/mcmc_mock_alt/mcmc_mpi/run.sh
+++ codes/referee_questions/mcmc_mock_alt/mcmc_mpi/run.sh
@@ -4,7 +4,7 @@ rm ../data/mcmc_output/mcmc*
 make cleanall
 make
 
-N_procs=20;
+N_procs=16;
 N_steps=500000;
 
 time mpirun -n $N_procs ./bin/run_mcmc $N_steps
--- codes/referee_questions/mcmc_mock_alt/mcmc_mpi/src/mcmc.c
+++ codes/referee_questions/mcmc_mock_alt/mcmc_mpi/src/mcmc.c
@@ -134,18 +134,18 @@ STEP_DATA update_parameters(STEP_DATA p, gsl_rng * GSL_r){
     double delta;
     STEP_DATA p_new;
 
-    // double thin_r0_sigma = 0.05;
-    // double thin_z0_sigma = 0.005;
-    // double thick_r0_sigma = 0.05;
-    // double thick_z0_sigma = 0.005;
-    // double ratio_thick_thin_sigma = 0.002;
+    double thin_r0_sigma = 0.05;
+    double thin_z0_sigma = 0.005;
+    double thick_r0_sigma = 0.05;
+    double thick_z0_sigma = 0.005;
+    double ratio_thick_thin_sigma = 0.002;
 
     /* try alternate step sizes */
-    double thin_r0_sigma = 0.2;
-    double thin_z0_sigma = 0.01;
-    double thick_r0_sigma = 0.25;
-    double thick_z0_sigma = 0.025;
-    double ratio_thick_thin_sigma = 0.05;
+    // double thin_r0_sigma = 0.2;
+    // double thin_z0_sigma = 0.01;
+    // double thick_r0_sigma = 0.25;
+    // double thick_z0_sigma = 0.025;
+    // double ratio_thick_thin_sigma = 0.05;
 
     /* change the position based on Gaussian distributions.  */
     delta = gsl_ran_gaussian(GSL_r, thin_r0_sigma);
