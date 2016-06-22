16416778083ad1a429d277cd53d38525d5975973
aszewciw <aszewci@gmail.com>
2016-06-22 15:20:00 -0500

    jio

--- codes/referee_questions/mcmc_density/mcmc/prepare_mcmc/run.sh
+++ codes/referee_questions/mcmc_density/mcmc/prepare_mcmc/run.sh
@@ -2,8 +2,10 @@
 
 rm ../data/rbins/*.dat
 rm ../data/model_positions/*.dat
+rm ../data/mock_density/*.dat
 
 N_bins=5;
 
 python choose_r_values.py $N_bins
 python separate_randoms.py
+python mock_density.py
