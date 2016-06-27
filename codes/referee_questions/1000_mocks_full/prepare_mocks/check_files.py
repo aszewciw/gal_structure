cf9323224c0626d92778be83b82bd645652f1069
aszewciw <aszewci@gmail.com>
2016-06-27 10:34:48 -0500

    ijij

---
+++ codes/referee_questions/1000_mocks_full/prepare_mocks/check_files.py
@@ -0,0 +1,30 @@
+import pickle, sys, os
+import numpy as np
+
+rawdata_dir = '../../data/'
+data_dir    = './data/'
+
+def main():
+
+    input_filename = rawdata_dir + 'todo_list.dat'
+    sys.stderr.write('Loading from file {} ...\n'.format(input_filename))
+    input_file     = open(input_filename, 'rb')
+    todo_list      = pickle.load(input_file)
+    input_file.close()
+
+    mock_num = np.arange(1000) + 1
+
+    for i in mock_num:
+
+        mock_dir = data_dir + 'mock_' str(i)
+
+        for p in todo_list:
+
+            filename = 'mock_' + p.ID + '.xyz.dat'
+
+            if not os.path.isfile(filename):
+                sys.stderr.write('Error: ' + filename + ' does not exist.\n')
+
+
+if __name__ == '__main__':
+    main()
