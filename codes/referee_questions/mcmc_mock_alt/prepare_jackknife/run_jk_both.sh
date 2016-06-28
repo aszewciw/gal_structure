57a6435ccc7eadf020d0ded9f6048f52beb31df0
aszewciw <aszewci@gmail.com>
2016-06-28 16:55:03 -0500

    jiojoijio

--- codes/referee_questions/mcmc_mock_alt/prepare_jackknife/run_jk_both.sh
+++ codes/referee_questions/mcmc_mock_alt/prepare_jackknife/run_jk_both.sh
@@ -1,15 +1,16 @@
-#! /usr/bin/bash
-
-bash run_jk_mock.sh
-bash run_jk_uniform.sh
+#!/usr/bin/bash
 
 rm ../data/rbins/*.dat
 python set_rbins.py
 
 rm ../data/jackknife/mock*
+rm ../data/jackknife/uniform*
 rm counts
 
 icc -Wall -xHost -O3 -vec_report2 counts.c -o counts
 
 python prepare_mock_jackknife.py
 python mock_error_jk.py
+
+python prepare_uniform_jackknife.py
+python uniform_error_jk.py
