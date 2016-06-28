1969e55ab23c58e5ab3b5dd84646bf78309e0c27
aszewciw <aszewci@gmail.com>
2016-06-28 11:49:36 -0500

    pair errors

---
+++ codes/referee_questions/1000_mocks_full/errors_pairs/run.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+# remove directories
+rm -rf ./data/mock*
+
+# make directories
+n=1;
+N_mocks=1000;
+while [ "$n" -le "$N_mocks" ]; do
+    mkdir "./data/mock_$n"
+    n=`expr "$n" + 1`;
+done
+
+rm mock_pair_count
+icc -Wall -xHost -O3 -vec_report2 mock_pair_count.c -o mock_pair_count
+
+time python mock_pair_count.py $N_mocks
+
+rm -rf ./data/mean_var_std
+mkdir ./data/mean_var_std
+time python average.py $N_mocks
