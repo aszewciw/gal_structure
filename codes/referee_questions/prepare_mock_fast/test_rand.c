427a1459c7b63faa09b14016161c159b19b29daa
aszewciw <aszewci@gmail.com>
2016-08-16 15:58:26 -0500

    changin seed

--- codes/referee_questions/prepare_mock_fast/test_rand.c
+++ codes/referee_questions/prepare_mock_fast/test_rand.c
@@ -15,16 +15,14 @@ int main( int argc, char **argv ){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     time_t t;
-    srand((unsigned) time(&t));
-
-    fprintf(stderr, "%lld\n", (long long) time(NULL));
+    srand((unsigned) time(&t) + (1+rank));
 
     // double proc_rnd = pow((double)rand() / (double)RAND_MAX, rank);
     // srand(proc_rnd);
 
-    // proc_rnd = (double)rand() / (double)RAND_MAX;
+    double proc_rnd = (double)rand() / (double)RAND_MAX;
 
-    // fprintf(stderr, "Process %d has initial random as %lf\n", rank, proc_rnd);
+    fprintf(stderr, "Process %d has initial random as %lf\n", rank, proc_rnd);
     MPI_Finalize();
     return EXIT_SUCCESS;
 }
