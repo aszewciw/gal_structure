d1db057364954ba73a25fc741e74162b431bba57
aszewciw <aszewci@gmail.com>
2016-08-17 14:45:11 -0500

    renamed script with compile lines

---
+++ codes/referee_questions/prepare_mock_fast/run_compile.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+rm ./data/*.dat
+
+make cleanall
+make
+
+nprocs=16;
+nstars=1000000;
+
+time mpirun -n $nprocs ./bin/make_galaxy $nstars
+python clean_mocks.py $nprocs
+
+rm ./data/proc*
