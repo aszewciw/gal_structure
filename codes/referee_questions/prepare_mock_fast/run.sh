4aa538d5dc4dbd9784c132df3d8facc766bd212a
aszewciw <aszewci@gmail.com>
2016-08-17 10:43:59 -0500

    testing multiproc'd galaxy creation

---
+++ codes/referee_questions/prepare_mock_fast/run.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+rm ./data/*.dat
+
+make cleanall
+make
+
+nprocs=32;
+nstars=1000000;
+
+time mpirun -n $nprocs ./bin/make_galaxy $nstars
+# python clean_mocks.py
+
+# rm ./data/temp*
