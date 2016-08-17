b58216410f5b64d87edd91d7b98ecf0d25366488
aszewciw <aszewci@gmail.com>
2016-08-17 14:44:50 -0500

    script without recompiling

--- codes/referee_questions/prepare_mock_fast/run.sh
+++ codes/referee_questions/prepare_mock_fast/run.sh
@@ -2,13 +2,10 @@
 
 rm ./data/*.dat
 
-make cleanall
-make
-
 nprocs=16;
 nstars=1000000;
 
 time mpirun -n $nprocs ./bin/make_galaxy $nstars
 python clean_mocks.py $nprocs
 
-# rm ./data/proc*
+rm ./data/proc*
