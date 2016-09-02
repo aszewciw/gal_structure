8982f156f231fc821e814be450d6d6019a560111
aszewciw <aszewci@gmail.com>
2016-09-02 08:25:56 -0500

    changin jk file

--- qq_code/qq_mock/data_prepare/prepare_data_jackknife.py
+++ qq_code/qq_mock/data_prepare/prepare_data_jackknife.py
@@ -7,8 +7,7 @@ This is incredibly dumb, but I need to do some manipulation to use his files...
 import sys, math, pickle, random
 import config
 import numpy as np
-class Star:
-    pass
+
 #------------------------------------------------------------------------------
 def main():
 
@@ -29,27 +28,28 @@ def main():
         star_filename = config.mock_dir + 'mock_' + p.ID + '.xyz.dat'
         x,y,z = np.genfromtxt(star_filename, unpack=True, skip_header=1)
         N_stars = len(x)
+        weight = np.ones(N_stars)
 
-        for i in range(N_stars):
-            s = Star()
-            s.cartesian_x = x[i]
-            s.cartesian_y = y[i]
-            s.cartesian_z = z[i]
-            s.weight = 1.0
+        # for i in range(N_stars):
+        #     s = Star()
+        #     s.cartesian_x = x[i]
+        #     s.cartesian_y = y[i]
+        #     s.cartesian_z = z[i]
+        #     s.weight = 1.0
 
-            star_list.append(s)
+        #     star_list.append(s)
 
-        # random shuffle
-        random.shuffle(star_list)
+        # # random shuffle
+        # random.shuffle(star_list)
 
         # first output a full shuffled data set
         output_filename = config.data_dir + 'star_' + p.ID + '_jk_all.dat'
         output_file = open(output_filename, 'w')
         # first output the total number of points
-        output_file.write('{}\n'.format(len(star_list)))
-        for s in star_list:
+        output_file.write('{}\n'.format(N_stars)
+        for i in range(N_stars:
             output_file.write('{}\t{}\t{}\t{}\n'
-                              .format(s.cartesian_x, s.cartesian_y, s.cartesian_z, s.weight))
+                              .format(x[i], y[i], z[i], weight[i]))
         output_file.close()
 
 
@@ -59,15 +59,24 @@ def main():
         N = int(len(star_list) / N_jackknife)
 
         for i in range(N_jackknife):
-            output_list = star_list[:(N * i)] + star_list[(N * i + N):]
+            # output_list = star_list[:(N * i)] + star_list[(N * i + N):]
+            cartesian_x = x[:(N*i)]
+            np.append(cartesian_x, x[(N*i+N):])
+            cartesian_y = y[:(N*i)]
+            np.append(cartesian_y, y[(N*i+N):])
+            cartesian_z = z[:(N*i)]
+            np.append(cartesian_z, z[(N*i+N):])
+            w = weight[:(N*i)]
+            np.append(w, weight[(N*i+N):])
 
+            N_stars_jk = len(cartesian_x)
             output_filename = config.data_dir + 'star_' + p.ID + '_jk_' + str(i) + '.dat'
             output_file = open(output_filename, 'w')
             # first output the total number of points
-            output_file.write('{}\n'.format(len(output_list)))
-            for s in output_list:
+            output_file.write('{}\n'.format(N_stars_jk))
+            for j in range(N_stars_jk):
                 output_file.write('{}\t{}\t{}\t{}\n'
-                                  .format(s.cartesian_x, s.cartesian_y, s.cartesian_z, s.weight))
+                                  .format(cartesian_x[j], cartesian_y[j], cartesian_z[j], w[j]))
 
             output_file.close()
 
