c52e52196f125db36f4499839418a717940c01ee
aszewciw <aszewci@gmail.com>
2016-09-02 10:34:20 -0500

    less steps, new files

--- qq_code/mcmc_mock/mcmc_mpi/run.sh
+++ qq_code/mcmc_mock/mcmc_mpi/run.sh
@@ -4,12 +4,12 @@ make cleanall
 make
 
 N_procs=20;
-N_steps=300000;
+N_steps=200000;
 params=1;
-filename=mcmc_result.dat;
+filename=mcmc_result_2.dat;
 
 rm ../data/mcmc_output/$filename
 
 time mpirun -n $N_procs ./bin/run_mcmc $N_steps $params $filename
 
-# python plot_disk_params.py
+# python plot_disk_params.python
