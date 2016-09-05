944e561c3331e8d310de43d2dc21052db7f8f50d
aszewciw <aszewci@gmail.com>
2016-09-05 12:44:50 -0500

    more procs, new filename

--- qq_code/mcmc_mock/mcmc_mpi/run.sh
+++ qq_code/mcmc_mock/mcmc_mpi/run.sh
@@ -3,13 +3,13 @@
 make cleanall
 make
 
-N_procs=20;
+N_procs=28;
 N_steps=200000;
 params=1;
-filename=mcmc_result_3.dat;
+filename=mcmc_result_4.dat;
 
 rm ../data/mcmc_output/$filename
 
 time mpirun -n $N_procs ./bin/run_mcmc $N_steps $params $filename
 
-# python plot_disk_params.python
+# python plot_disk_params.py
