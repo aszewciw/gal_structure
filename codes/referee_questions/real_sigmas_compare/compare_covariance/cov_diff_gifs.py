d2bc7264e02861cbca3cc07b55335f7895bff6bc
aszewciw <aszewci@gmail.com>
2016-09-15 22:22:21 -0500

    division instead of frac difference

--- codes/referee_questions/real_sigmas_compare/compare_covariance/cov_diff_gifs.py
+++ codes/referee_questions/real_sigmas_compare/compare_covariance/cov_diff_gifs.py
@@ -90,23 +90,28 @@ def main():
         # Calculate fiducial covariance
         cov_1 = DF_1.cov()
 
-        cov_frac = (cov_1 - cov_2) / cov_1
+        # cov_frac = (cov_1 - cov_2) / cov_1
+        cov_div = cov_2 / cov_1
 
         # plot heatmap of matrix
         plt.clf()
         sns.set(style="white")
-        mask = np.zeros_like(cov_frac, dtype=np.bool)
+        # mask = np.zeros_like(cov_frac, dtype=np.bool)
+        mask = np.zeros_like(cov_div, dtype=np.bool)
         mask[np.triu_indices_from(mask)] = True
         f, ax = plt.subplots(figsize=(11, 9))
         cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
-        sns.heatmap(cov_frac, mask=mask, cmap=cmap,square=True, annot=True,
+        # sns.heatmap(cov_frac, mask=mask, cmap=cmap,square=True, annot=True,
+        #             xticklabels=col_names, yticklabels=col_names, linewidths=.5,
+        #             cbar_kws={"shrink": .5}, ax=ax, vmin=-1.0, vmax=1.0)
+        sns.heatmap(cov_div, mask=mask, cmap=cmap,square=True, annot=True,
                     xticklabels=col_names, yticklabels=col_names, linewidths=.5,
                     cbar_kws={"shrink": .5}, ax=ax, vmin=-1.0, vmax=1.0)
-        plt.title('Covariance matrix fractional difference for l.o.s. ' + ID, fontsize=20)
+        plt.title('Covariance matrix ratio for l.o.s. ' + ID, fontsize=20)
         plt.xlabel('Bin Center (kpc)', fontsize=18)
         plt.ylabel('Bin Center (kpc)', fontsize=18)
 
-        fig_name = plots_dir + 'cov_matrix_diff_12' + ID + '.png'
+        fig_name = plots_dir + 'cov_matrix_ratio_12' + ID + '.png'
         plt.savefig(fig_name)
         png_list.append(fig_name)
 
