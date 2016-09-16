90297712c8b7114a027e20b2c82354d3c9037a29
aszewciw <aszewci@gmail.com>
2016-09-16 07:07:58 -0500

    21

--- codes/referee_questions/real_sigmas_compare/compare_covariance/cov_diff_gifs.py
+++ codes/referee_questions/real_sigmas_compare/compare_covariance/cov_diff_gifs.py
@@ -79,11 +79,11 @@ def main():
         # Each row is a mock, each column is a bin
 
         # Load fiducial
-        mod0_filename = mod0_dir + 'normed_counts_all_' + ID + '.dat'
-        DF_0 = pd.read_csv(mod0_filename, sep='\s+', names=col_names)
+        # mod0_filename = mod0_dir + 'normed_counts_all_' + ID + '.dat'
+        # DF_0 = pd.read_csv(mod0_filename, sep='\s+', names=col_names)
 
-        # Calculate fiducial covariance
-        cov_0 = DF_0.cov()
+        # # Calculate fiducial covariance
+        # cov_0 = DF_0.cov()
 
 
         # Load model 1
@@ -95,22 +95,22 @@ def main():
 
 
         # Load model 2
-        # mod2_filename = mod2_dir + 'normed_counts_all_' + ID + '.dat'
-        # DF_2 = pd.read_csv(mod2_filename, sep='\s+', names=col_names)
+        mod2_filename = mod2_dir + 'normed_counts_all_' + ID + '.dat'
+        DF_2 = pd.read_csv(mod2_filename, sep='\s+', names=col_names)
 
-        # # Calculate model 2 covariance
-        # cov_2 = DF_2.cov()
+        # Calculate model 2 covariance
+        cov_2 = DF_2.cov()
 
 
 
         # cov_div = cov_1 / cov_0
-        cov_frac = (cov_1 - cov_0) / cov_0
+        # cov_frac = (cov_1 - cov_0) / cov_0
 
         # cov_div = cov_2 / cov_0
         # cov_frac = (cov_2 - cov_0) / cov_0
 
         # cov_div = cov_2 / cov_1
-        # cov_frac = (cov_2 - cov_1) / cov_1
+        cov_frac = (cov_2 - cov_1) / cov_1
 
         # plot heatmap of matrix
         plt.clf()
@@ -131,16 +131,16 @@ def main():
         plt.xlabel('Bin Center (kpc)', fontsize=18)
         plt.ylabel('Bin Center (kpc)', fontsize=18)
 
-        fig_name = plots_dir + 'cov_matrix_diff_10' + ID + '.png'
+        # fig_name = plots_dir + 'cov_matrix_diff_10' + ID + '.png'
         # fig_name = plots_dir + 'cov_matrix_diff_20' + ID + '.png'
-        # fig_name = plots_dir + 'cov_matrix_diff_21' + ID + '.png'
+        fig_name = plots_dir + 'cov_matrix_diff_21' + ID + '.png'
 
         plt.savefig(fig_name)
         png_list.append(fig_name)
 
-    gif_name = plots_dir + 'cov_matrix_10.gif'
+    # gif_name = plots_dir + 'cov_matrix_10.gif'
     # gif_name = plots_dir + 'cov_matrix_20.gif'
-    # gif_name = plots_dir + 'cov_matrix_21.gif'
+    gif_name = plots_dir + 'cov_matrix_21.gif'
 
     GIF_MOVIE(png_list, gif_name, removef=True)
 
