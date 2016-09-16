24d0f24570d20b85f7aeeba249dfa39b9495514a
aszewciw <aszewci@gmail.com>
2016-09-16 06:37:59 -0500

    removing commented section

--- codes/referee_questions/real_sigmas_compare/compare_covariance/frac_cov_diff_gifs.py
+++ codes/referee_questions/real_sigmas_compare/compare_covariance/frac_cov_diff_gifs.py
@@ -77,108 +77,108 @@ def main():
     # Calculate covariance matrix for each l.o.s.
     for ID in ID_list:
 
-        # # Load counts from 1000 mocks with pandas
-        # # Each row is a mock, each column is a bin
+        # Load counts from 1000 mocks with pandas
+        # Each row is a mock, each column is a bin
 
-        # # Load fiducial
-        # mod0_filename = mod0_dir + 'normed_counts_all_' + ID + '.dat'
-        # DF_0 = pd.read_csv(mod0_filename, sep='\s+')
+        # Load fiducial
+        mod0_filename = mod0_dir + 'normed_counts_all_' + ID + '.dat'
+        DF_0 = pd.read_csv(mod0_filename, sep='\s+')
 
-        # # Calculate fiducial covariance
-        # cov_0 = DF_0.cov()
+        # Calculate fiducial covariance
+        cov_0 = DF_0.cov()
 
-        # # Load model 1
-        # mod1_filename = mod1_dir + 'normed_counts_all_' + ID + '.dat'
-        # DF_1 = pd.read_csv(mod1_filename, sep='\s+')
+        # Load model 1
+        mod1_filename = mod1_dir + 'normed_counts_all_' + ID + '.dat'
+        DF_1 = pd.read_csv(mod1_filename, sep='\s+')
 
-        # # Calculate fiducial covariance
-        # cov_1 = DF_1.cov()
+        # Calculate fiducial covariance
+        cov_1 = DF_1.cov()
 
-        # # Load model 2
-        # mod2_filename = mod2_dir + 'normed_counts_all_' + ID + '.dat'
-        # DF_2 = pd.read_csv(mod2_filename, sep='\s+')
+        # Load model 2
+        mod2_filename = mod2_dir + 'normed_counts_all_' + ID + '.dat'
+        DF_2 = pd.read_csv(mod2_filename, sep='\s+')
 
-        # # Calculate model 2 covariance
-        # cov_2 = DF_2.cov()
-
-
-        # # Get means of different columns
-        # means_0 = DF_0.mean().values
-        # means_1 = DF_1.mean().values
-        # means_2 = DF_2.mean().values
-        # N = len(means_0)
-
-        # # Turn means into a matrix
-        # means_mat_0 = np.zeros((N,N))
-        # means_mat_1 = np.zeros((N,N))
-        # means_mat_1 = np.zeros((N,N))
-        # for i in range(N):
-        #     for j in range(N):
-        #         means_mat_0 = means_0[i]*means_0[j]
-        #         means_mat_1 = means_1[i]*means_1[j]
-        #         means_mat_2 = means_2[i]*means_2[j]
+        # Calculate model 2 covariance
+        cov_2 = DF_2.cov()
 
 
-        # # Calculate sigmas / means of covariance matrices
-        # frac_cov_0 = cov_0.values/means_mat_0
-        # frac_cov_1 = cov_1.values/means_mat_1
-        # frac_cov_2 = cov_2.values/means_mat_2
+        # Get means of different columns
+        means_0 = DF_0.mean().values
+        means_1 = DF_1.mean().values
+        means_2 = DF_2.mean().values
+        N = len(means_0)
 
-        # # Determine fractional differences in fractional covariance matrices
-        # # Oy vey
-        # frac_diff_10 = (frac_cov_1 - frac_cov_0) / frac_cov_0
-        # frac_diff_20 = (frac_cov_2 - frac_cov_0) / frac_cov_0
-        # frac_diff_21 = (frac_cov_2 - frac_cov_1) / frac_cov_1
+        # Turn means into a matrix
+        means_mat_0 = np.zeros((N,N))
+        means_mat_1 = np.zeros((N,N))
+        means_mat_1 = np.zeros((N,N))
+        for i in range(N):
+            for j in range(N):
+                means_mat_0 = means_0[i]*means_0[j]
+                means_mat_1 = means_1[i]*means_1[j]
+                means_mat_2 = means_2[i]*means_2[j]
+
+
+        # Calculate sigmas / means of covariance matrices
+        frac_cov_0 = cov_0.values/means_mat_0
+        frac_cov_1 = cov_1.values/means_mat_1
+        frac_cov_2 = cov_2.values/means_mat_2
+
+        # Determine fractional differences in fractional covariance matrices
+        # Oy vey
+        frac_diff_10 = (frac_cov_1 - frac_cov_0) / frac_cov_0
+        frac_diff_20 = (frac_cov_2 - frac_cov_0) / frac_cov_0
+        frac_diff_21 = (frac_cov_2 - frac_cov_1) / frac_cov_1
 
         # plot first heatmap
-        # plt.clf()
-        # sns.set(style="white")
-        # mask = np.zeros_like(frac_diff_10, dtype=np.bool)
-        # mask[np.triu_indices_from(mask)] = True
-        # f, ax = plt.subplots(figsize=(11, 9))
-        # cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
-        # sns.heatmap(frac_diff_10, mask=mask, cmap=cmap,square=True, annot=True,
-        #             xticklabels=col_names, yticklabels=col_names, linewidths=.5,
-        #             cbar_kws={"shrink": .5}, ax=ax, vmin=-2.0, vmax=2.0)
-        # plt.title('Fractional difference in fractional covariance for l.o.s. ' + ID, fontsize=20)
-        # plt.xlabel('Bin Center (kpc)', fontsize=18)
-        # plt.ylabel('Bin Center (kpc)', fontsize=18)
+        plt.clf()
+        sns.set(style="white")
+        mask = np.zeros_like(frac_diff_10, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+        f, ax = plt.subplots(figsize=(11, 9))
+        cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
+        sns.heatmap(frac_diff_10, mask=mask, cmap=cmap,square=True, annot=True,
+                    xticklabels=col_names, yticklabels=col_names, linewidths=.5,
+                    cbar_kws={"shrink": .5}, ax=ax, vmin=-2.0, vmax=2.0)
+        plt.title('Fractional difference in fractional covariance for l.o.s. ' + ID, fontsize=20)
+        plt.xlabel('Bin Center (kpc)', fontsize=18)
+        plt.ylabel('Bin Center (kpc)', fontsize=18)
         fig_name = plots_dir + 'frac_cov_diff_10' + ID + '.png'
-        # plt.savefig(fig_name)
+        plt.savefig(fig_name)
         png_list_10.append(fig_name)
 
         # plot second heatmap
-        # plt.clf()
-        # sns.set(style="white")
-        # mask = np.zeros_like(frac_diff_20, dtype=np.bool)
-        # mask[np.triu_indices_from(mask)] = True
-        # f, ax = plt.subplots(figsize=(11, 9))
-        # cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
-        # sns.heatmap(frac_diff_20, mask=mask, cmap=cmap,square=True, annot=True,
-        #             xticklabels=col_names, yticklabels=col_names, linewidths=.5,
-        #             cbar_kws={"shrink": .5}, ax=ax, vmin=-2.0, vmax=2.0)
-        # plt.title('Fractional difference in fractional covariance for l.o.s. ' + ID, fontsize=20)
-        # plt.xlabel('Bin Center (kpc)', fontsize=18)
-        # plt.ylabel('Bin Center (kpc)', fontsize=18)
+        plt.clf()
+        sns.set(style="white")
+        mask = np.zeros_like(frac_diff_20, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+        f, ax = plt.subplots(figsize=(11, 9))
+        cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
+        sns.heatmap(frac_diff_20, mask=mask, cmap=cmap,square=True, annot=True,
+                    xticklabels=col_names, yticklabels=col_names, linewidths=.5,
+                    cbar_kws={"shrink": .5}, ax=ax, vmin=-2.0, vmax=2.0)
+        plt.title('Fractional difference in fractional covariance for l.o.s. ' + ID, fontsize=20)
+        plt.xlabel('Bin Center (kpc)', fontsize=18)
+        plt.ylabel('Bin Center (kpc)', fontsize=18)
         fig_name = plots_dir + 'frac_cov_diff_20' + ID + '.png'
-        # plt.savefig(fig_name)
+        plt.savefig(fig_name)
         png_list_20.append(fig_name)
 
         # plot heatmap of matrix
-        # plt.clf()
-        # sns.set(style="white")
-        # mask = np.zeros_like(frac_diff_21, dtype=np.bool)
-        # mask[np.triu_indices_from(mask)] = True
-        # f, ax = plt.subplots(figsize=(11, 9))
-        # cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
-        # sns.heatmap(frac_diff_21, mask=mask, cmap=cmap,square=True, annot=True,
-        #             xticklabels=col_names, yticklabels=col_names, linewidths=.5,
-        #             cbar_kws={"shrink": .5}, ax=ax, vmin=-2.0, vmax=2.0)
-        # plt.title('Fractional difference in fractional covariance for l.o.s. ' + ID, fontsize=20)
-        # plt.xlabel('Bin Center (kpc)', fontsize=18)
-        # plt.ylabel('Bin Center (kpc)', fontsize=18)
+        plt.clf()
+        sns.set(style="white")
+        mask = np.zeros_like(frac_diff_21, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+        f, ax = plt.subplots(figsize=(11, 9))
+        cmap = sns.diverging_palette(145, 280, s=85, l=25, n=7, as_cmap=True)
+        sns.heatmap(frac_diff_21, mask=mask, cmap=cmap,square=True, annot=True,
+                    xticklabels=col_names, yticklabels=col_names, linewidths=.5,
+                    cbar_kws={"shrink": .5}, ax=ax, vmin=-2.0, vmax=2.0)
+        plt.title('Fractional difference in fractional covariance for l.o.s. ' + ID, fontsize=20)
+        plt.xlabel('Bin Center (kpc)', fontsize=18)
+        plt.ylabel('Bin Center (kpc)', fontsize=18)
         fig_name = plots_dir + 'frac_cov_diff_21' + ID + '.png'
-        # plt.savefig(fig_name)
+        plt.savefig(fig_name)
         png_list_21.append(fig_name)
 
     print("Plots finished. Making gifs...\n")
