0735a2131f7fc3125798bbaeb1607f102ae896b6
aszewciw <aszewci@gmail.com>
2016-09-22 22:29:50 -0500

    commenting out plotter

--- codes/referee_questions/mcmc_mock/mcmc_mpi/run.sh
+++ codes/referee_questions/mcmc_mock/mcmc_mpi/run.sh
@@ -4,10 +4,10 @@ make cleanall
 make
 
 N_procs=20;
-filename="../data/mcmc_output/mcmc_test.dat";
+# filename="../data/mcmc_output/mcmc_test.dat";
 
 rm $filename
 
 time mpirun -n $N_procs ./bin/run_mcmc -f $filename
 
-python plot_disk_params.py
+# python plot_disk_params.py
