c2194f5d8bc25f6725b3675e1698805730321407
aszewciw <aszewci@gmail.com>
2016-09-27 13:55:59 -0500

    prints

--- codes/referee_questions/mcmc_new_cov/mcmc_mpi/src/errors.c
+++ codes/referee_questions/mcmc_new_cov/mcmc_mpi/src/errors.c
@@ -18,6 +18,7 @@ double calculate_chi2(POINTING *p, int N_bins, int lower_ind, int upper_ind){
     double corr_model_j, corr_model_k;
     double corr_data_j, corr_data_k;
     double sigma_j, sigma_k;
+    double chi2_temp;
 
     for(i = lower_ind; i < upper_ind; i++){
 
@@ -46,9 +47,14 @@ double calculate_chi2(POINTING *p, int N_bins, int lower_ind, int upper_ind){
                 sigma_k = ( corr_model_k * p[i].rbin[k].frac_error );
 
                 /* add contribution to chi2 from correlation matrix element */
-                chi2 += ( ( ( corr_data_j - corr_model_j ) / sigma_j )
+                chi2_temp = ( ( ( corr_data_j - corr_model_j ) / sigma_j )
                     * ( ( corr_data_k - corr_model_k ) / sigma_k )
                     * p[i].invcor_row[k].invcor_col[j] );
+
+                if (chi2_temp>1000.0){
+                    fprintf(stderr, "i: %d, j: %d, k: %d, chi2: %lf \n", i, j, k, chi2_temp);
+                }
+                chi2 += chi2_temp;
             }
 
         }
