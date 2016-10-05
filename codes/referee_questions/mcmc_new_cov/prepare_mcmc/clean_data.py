ad903bbb96b9ade2fc431c637a4ef0f91ae681a3
aszewciw <aszewci@gmail.com>
2016-10-05 09:52:50 -0500

    adding lines to use 10000 mocks

--- codes/referee_questions/mcmc_new_cov/prepare_mcmc/clean_data.py
+++ codes/referee_questions/mcmc_new_cov/prepare_mcmc/clean_data.py
@@ -15,38 +15,32 @@ def main():
         usecols=[0], dtype=str)
 
     # Repack file of just pointing IDs
-    outfile   = rawdata_dir + 'pointing_ID.dat'
-    with open(outfile, 'w') as f:
-        f.write(str(len(ID)))
-        f.write('\n')
-        for i in range(len(ID)):
-            f.write("{}\n".format(ID[i]))
+    # outfile   = rawdata_dir + 'pointing_ID.dat'
+    # with open(outfile, 'w') as f:
+    #     f.write(str(len(ID)))
+    #     f.write('\n')
+    #     for i in range(len(ID)):
+    #         f.write("{}\n".format(ID[i]))
 
 
     for p in ID:
 
-        # Repack file containing Z, R, and W=1.0 only
-        ZRW_file = uni_dir + 'uniform_' + p + '.ascii.dat'
-        Z, R, W  = np.genfromtxt(ZRW_file, unpack=True, skip_header=1,
-            usecols=[5, 6, 10], dtype=None)
-        N_points = len(Z)
-        outfile  = zrw_dir + 'uniform_ZRW_' + p + '.dat'
-        with open(outfile, 'w') as f:
-            f.write(str(N_points))
-            f.write('\n')
-            for i in range(N_points):
-                f.write("{} {} {}\n".format(
-                    str(Z[i]), str(R[i]), str(W[i])))
+        # # Repack file containing Z, R, and W=1.0 only
+        # ZRW_file = uni_dir + 'uniform_' + p + '.ascii.dat'
+        # Z, R, W  = np.genfromtxt(ZRW_file, unpack=True, skip_header=1,
+        #     usecols=[5, 6, 10], dtype=None)
+        # N_points = len(Z)
+        # outfile  = zrw_dir + 'uniform_ZRW_' + p + '.dat'
+        # with open(outfile, 'w') as f:
+        #     f.write(str(N_points))
+        #     f.write('\n')
+        #     for i in range(N_points):
+        #         f.write("{} {} {}\n".format(
+        #             str(Z[i]), str(R[i]), str(W[i])))
 
         # Repack files containing sigma/DD
         sigma_file = sigma_dir + 'stats_' + p + '.dat'
-        # DD, std = np.genfromtxt(sigma_file, unpack=True, usecols=[3,5])
-        DD, std = np.genfromtxt(sigma_file, unpack=True, usecols=[0,2])
-        # frac_std = np.zeros(len(DD))
-        # for i in range(len(DD)):
-        #     if DD[i] == 0.0:
-        #         continue
-        #     frac_std[i] = std[i] / DD[i]
+        DD, std = np.genfromtxt(sigma_file, unpack=True, usecols=[3,5])
         frac_std = std/DD
 
         outfile = errors_dir + 'frac_error_' + p + '.dat'
