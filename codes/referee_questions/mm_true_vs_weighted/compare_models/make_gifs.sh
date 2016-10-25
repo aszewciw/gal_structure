e2d556eca3e91fd9427369f994ba8e2588d62296
aszewciw <aszewci@gmail.com>
2016-10-25 17:13:56 -0500

    bleh

--- codes/referee_questions/mm_true_vs_weighted/compare_models/make_gifs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-rm ./plots/*.gif
-rm ./plots/*.png
-
-# python mm_compare_gif.py 1000
-python mm_compare_gif.py 10000
--- codes/referee_questions/mm_true_vs_weighted/compare_models/mm_compare.py
+++ codes/referee_questions/mm_true_vs_weighted/compare_models/mm_compare.py
@@ -14,6 +14,14 @@ Make a gif from the files.
 
 def main():
 
+    # CL Input
+    # star_factor = N_random / N_data in each l.o.s.
+    elements_needed = int(2)
+    args_array      = np.array(sys.argv)
+    N_args          = len(args_array)
+    assert(N_args == elements_needed)
+    N     = int(args_array[1])
+
     # Load list of pointing IDs
     todo_file = rawdata_dir + 'todo_list.ascii.dat'
     ID_list   = np.genfromtxt(todo_file, skip_header=1, usecols=[0], unpack=True,
@@ -31,11 +39,11 @@ def main():
     # Create list of png's for use in making gif
     png_list = []
 
-    org_patch = mpatches.Patch(color='#CC4F1B', label=r'$MM_{mean}$')
-    black_patch = mpatches.Patch(color='black', label=r'$MM_{10}$')
-    green_patch = mpatches.Patch(color='green', label=r'$MM_{50}$')
-    blue_patch = mpatches.Patch(color='blue', label=r'$MM_{100}$')
-    red_patch = mpatches.Patch(color='red', label=r'$MM_{1000}$')
+    # org_patch = mpatches.Patch(color='#CC4F1B', label=r'$MM_{mean}$')
+    # black_patch = mpatches.Patch(color='black', label=r'$MM_{10}$')
+    # green_patch = mpatches.Patch(color='green', label=r'$MM_{50}$')
+    # blue_patch = mpatches.Patch(color='blue', label=r'$MM_{100}$')
+    # red_patch = mpatches.Patch(color='red', label=r'$MM_{1000}$')
 
     chi2_0_tt      = 0.0
     chi2_0_et      = 0.0
@@ -67,24 +75,24 @@ def main():
         mm_mean_0, std_0 = np.genfromtxt(mock_file, unpack=True, usecols=[0,2])
 
         # Load weighted random counts for model 0
-        w_10_file = pairs0_dir + 'mm_10_' + ID + '.dat'
-        mm_0 = np.genfromtxt(w_10_file)
+        w_file = pairs0_dir + 'mm_' + str(N) + '_' + ID + '.dat'
+        mm_0 = np.genfromtxt(w_file)
 
         # Load stats for model 1
         mock_file = mod1_dir + 'stats_' + ID + '.dat'
         mm_mean_1, std_1 = np.genfromtxt(mock_file, unpack=True, usecols=[0,2])
 
         # Load weighted random counts for model 1
-        w_10_file = pairs1_dir + 'mm_10_' + ID + '.dat'
-        mm_1 = np.genfromtxt(w_10_file)
+        w_file = pairs1_dir + 'mm_' + str(N) + '_' + ID + '.dat'
+        mm_1 = np.genfromtxt(w_file)
 
         # Load stats for model 2
         mock_file = mod2_dir + 'stats_' + ID + '.dat'
         mm_mean_2, std_2 = np.genfromtxt(mock_file, unpack=True, usecols=[0,2])
 
         # Load weighted random counts for model 2
-        w_10_file = pairs2_dir + 'mm_10_' + ID + '.dat'
-        mm_2 = np.genfromtxt(w_10_file)
+        w_file = pairs2_dir + 'mm_' + str(N) + '_' + ID + '.dat'
+        mm_2 = np.genfromtxt(w_file)
 
         # Load dd counts from one mock
         dd_file = dd_dir + 'dd_' + ID + '.dat'
@@ -136,13 +144,6 @@ def main():
             chi2_2_ee_frac += ( (mm_2[i] - DD) / STD_2 )**2
 
 
-    # print(chi2_0_tt, chi2_1_tt, chi2_2_tt)
-    # print(chi2_0_et, chi2_1_et, chi2_2_et)
-    # print(chi2_0_te_fid, chi2_1_te_fid, chi2_2_te_fid)
-    # print(chi2_0_ee_fid, chi2_1_ee_fid, chi2_2_ee_fid)
-    # print(chi2_0_te_frac, chi2_1_te_frac, chi2_2_te_frac)
-    # print(chi2_0_ee_frac, chi2_1_ee_frac, chi2_2_ee_frac)
-
     tt      = np.array([chi2_0_tt, chi2_1_tt, chi2_2_tt])
     et      = np.array([chi2_0_et, chi2_1_et, chi2_2_et])
     te_fid  = np.array([chi2_0_te_fid, chi2_1_te_fid, chi2_2_te_fid])
@@ -164,48 +165,9 @@ def main():
     plt.plot(models, ee_frac, marker='p', label=r'$MM_{est}, \sigma_{est}$')
     plt.legend(numpoints=1, loc='upper left')
 
-# plt.clf()
-# time_labels = ['10 Myr', '100 Myr', '1 Gyr', '2 Gyr', '5 Gyr', '10 Gyr']
-# time_markers = ['*k', '*g', '*c', '*m', '*y', '*r']
-# plt.plot(color_per_bin, M_abs, 'r', label='Largest Star Color')
-# plt.plot(color_pop, M_abs, 'b', label='Population Color')
-# for i in range(len(color_array_pop)):
-#     plt.plot(color_array_pop[i], M_abs_array[i], time_markers[i], markersize=15, label=time_labels[i])
-#     plt.plot(color_array_largest[i], M_abs_array[i], time_markers[i], markersize=15)
-# plt.legend(numpoints=1, loc=4)
-# plt.xlabel('g - r color')
-# plt.ylabel('M(t) - M(0)')
-# plt.title('Color Magnitude for Passive Evolution')
-# plt.savefig('Problem3.png')
-# plt.clf()
-    fig_name = plots_dir + 'chi2.png'
+    fig_name = plots_dir + 'chi2_' + str(N) + '.png'
 
     plt.savefig(fig_name)
-
-        # plt.close()
-        # plt.clf()
-        # xmin = min(bin_centers)
-        # xmax = max(bin_centers)
-        # ymin = -1
-        # ymax = 1
-
-        # plt.title('Real mean (' + N_mocks + ' mocks) vs. weighted mean l.o.s. ' + ID, fontsize=20)
-        # plt.xlabel('Bin Center (kpc)', fontsize=18)
-        # plt.ylabel(r'$\frac{MM_{weighted}-MM_{mean}}{\sigma}$', fontsize=18)
-        # # plt.ylabel(r'$\frac{MM_{weighted}-MM_{mean}}{MM_{mean}}$', fontsize=18)
-        # # plt.semilogx(bin_centers, dd_mean, color='#CC4F1B')
-        # # plt.semilogx(bin_centers, dd_weighted, color='black')
-        # # plt.fill_between(bin_centers, dd_mean-std, dd_mean+std, alpha=0.5, edgecolor='#CC4F1B',
-        # #     facecolor='#FF9848')
-        # plt.semilogx(bin_centers, excess_10, color='black')
-        # plt.semilogx(bin_centers, excess_50, color='green')
-        # plt.semilogx(bin_centers, excess_100, color='blue')
-        # plt.semilogx(bin_centers, excess_1000, color='red')
-        # plt.axis([xmin, xmax, ymin, ymax])
-        # plt.legend(handles=[black_patch, green_patch, blue_patch, red_patch], loc='upper left')
-        # fig_name = plots_dir + 'real_vs_weighted_' + N_mocks + '_' + ID + '.png'
-        # plt.savefig(fig_name)
-        # png_list.append(fig_name)
 
 
 if __name__ == '__main__':
