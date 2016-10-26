00f7cf8ec848dbd8c9818015439ce91c815a3ccc
aszewciw <aszewci@gmail.com>
2016-10-25 23:21:04 -0500

    iij

--- codes/referee_questions/mcmc_new/mcmc_mpi/run.sh
+++ codes/referee_questions/mcmc_new/mcmc_mpi/run.sh
@@ -3,11 +3,12 @@
 make cleanall
 make
 
-N_procs=20;
-filename="../data/mcmc_output/mcmc_result.dat";
+N_procs=30;
+N_steps=200000;
+filename="../data/mcmc_output/mcmc_result_50random.dat";
 
 rm $filename
 
-time mpirun -n $N_procs ./bin/run_mcmc -f $filename
+time mpirun -n $N_procs ./bin/run_mcmc -f $filename -N_s $N_steps
 
 # python plot_disk_params.py
