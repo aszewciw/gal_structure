377ee478ea506a366ad349fa466dcc756e61a969
aszewciw <aszewci@gmail.com>
2016-10-28 11:21:36 -0500

    jiji

--- codes/referee_questions/mm_true_vs_weighted/compare_models/mm_compare_bins.py
+++ codes/referee_questions/mm_true_vs_weighted/compare_models/mm_compare_bins.py
@@ -171,12 +171,12 @@ def main():
     plt.xlabel('Bin Centers (kpc)')
     plt.ylabel(r'$\chi^2$')
 
-    plt.semilogx(bin_centers, chi2_0_true, color='red', marker='*', markersize=15, label='Model 0')
-    plt.semilogx(bin_centers, chi2_0_est, color='red', marker='s', markersize=15)
-    plt.semilogx(bin_centers, chi2_1_true, color='blue', marker='*', markersize=15, label='Model 1')
-    plt.semilogx(bin_centers, chi2_1_est, color='blue', markersize=15, marker='s')
-    plt.semilogx(bin_centers, chi2_2_true, color='green', marker='*', markersize=15, label='Model 2')
-    plt.semilogx(bin_centers, chi2_2_est, color='green', marker='s', markersize=15)
+    plt.semilogx(bin_centers, chi2_0_true, color='red', marker='o', markersize=5, label='Model 0')
+    plt.semilogx(bin_centers, chi2_0_est, color='red', marker='s', markersize=5)
+    plt.semilogx(bin_centers, chi2_1_true, color='blue', marker='o', markersize=5, label='Model 1')
+    plt.semilogx(bin_centers, chi2_1_est, color='blue', markersize=5, marker='s')
+    plt.semilogx(bin_centers, chi2_2_true, color='green', marker='o', markersize=5, label='Model 2')
+    plt.semilogx(bin_centers, chi2_2_est, color='green', marker='s', markersize=5)
 
     # plt.plot(models, tt, marker='*', label=r'$MM_{true}, \sigma_{true}$')
     # plt.plot(models, et, marker='^', label=r'$MM_{est}, \sigma_{true}$')
