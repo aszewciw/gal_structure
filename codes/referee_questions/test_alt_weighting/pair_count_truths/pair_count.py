d3ea86dffdae74ce035f5e722355ea272c7957dd
aszewciw <aszewci@gmail.com>
2016-11-04 09:52:02 -0500

    dir

---
+++ codes/referee_questions/test_alt_weighting/pair_count_truths/pair_count.py
@@ -0,0 +1,66 @@
+'''
+Loops over all SEGUE los and produces a file for each
+mock containing the normalized dd counts.
+'''
+
+from config import *
+import os, sys
+import numpy as np
+
+
+def main():
+
+    todo_file = rawdata_dir + 'todo_list.ascii.dat'
+    ID_list = np.genfromtxt(todo_file, skip_header=1, usecols=[0], unpack=True,
+                            dtype=str)
+    N_los = len(ID_list)
+
+    bins_file = data_dir + 'rbins.ascii.dat'
+    if not os.path.isfile(bins_file):
+        sys.stderr.write('Error: ' + bins_file + ' does not exist.\n')
+
+    for p in ID_list:
+
+        # First do counts for A
+        in_file = mockA_dir + 'mock_' + p + '.xyzw.dat'
+
+        if not os.path.isfile(in_file):
+            sys.stderr.write('Error: ' + in_file + ' does not exist.\n')
+            continue
+
+        output_file = data_dir + 'mod_A_mm_' + p + '.dat'
+
+        cmd = ( './pair_count ' + in_file + ' ' + bins_file
+            + ' > ' + output_file )
+        os.system(cmd)
+
+
+        # Next do B
+        in_file = mockB_dir + 'mock_' + p + '.xyzw.dat'
+
+        if not os.path.isfile(in_file):
+            sys.stderr.write('Error: ' + in_file + ' does not exist.\n')
+            continue
+
+        output_file = data_dir + 'mod_B_mm_' + p + '.dat'
+
+        cmd = ( './pair_count ' + in_file + ' ' + bins_file
+            + ' > ' + output_file )
+        os.system(cmd)
+
+
+        # Last do C
+        in_file = mockC_dir + 'mock_' + p + '.xyzw.dat'
+
+        if not os.path.isfile(in_file):
+            sys.stderr.write('Error: ' + in_file + ' does not exist.\n')
+            continue
+
+        output_file = data_dir + 'mod_C_mm_' + p + '.dat'
+
+        cmd = ( './pair_count ' + in_file + ' ' + bins_file
+            + ' > ' + output_file )
+        os.system(cmd)
+
+if __name__ == '__main__':
+    main()
