13c7f9e3574be3741dc1a90b25a4d031fecbf9c7
aszewciw <aszewci@gmail.com>
2016-11-15 17:09:29 -0600

    ijij

--- codes/referee_questions/check_Nmocks/compare_sigmas.py
+++ codes/referee_questions/check_Nmocks/compare_sigmas.py
@@ -2,10 +2,33 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import os
 
 mocks_dir = '../10000_mocks/errors_pairs/data/'
 todo_dir  = '../data/'
 rbins_dir = '../mcmc_mock/data/rbins/'
+
+#------------------------------------------------------------------------------#
+def GIF_MOVIE(files, output_gif, delay=60, repeat=True, removef=False):
+    """
+    Given a list if 'files', it creates a gif file, and deletes temp files.
+
+    Parameters
+    ----------
+    files: array_like
+            List of abs. paths to temporary figures
+
+    output_gif: str
+            Absolute path to output gif file.
+    """
+    loop = -1 if repeat else 0
+    os.system('convert -delay %d -loop %d %s %s' %( delay,loop," ".join(files), \
+        output_gif) )
+
+    if removef:
+        for fname in files: os.remove(fname)
+
+#------------------------------------------------------------------------------#
 
 def main():
 
@@ -17,14 +40,13 @@ def main():
     bin_cent = np.round(bin_cent, 3)
 
     Nmock_list = [50, 100, 500, 1000, 5000]
-    color_list = ['red', 'green', 'cyan', 'blue', 'black']
+    color_list = ['red', 'green', 'blue', 'cyan', 'magenta']
 
-    np.random.seed()
+    file_list = []
 
     for ID in ID_list:
 
-        if ID != '0':
-            continue
+        print('On pointing number ' + ID)
 
         counts_file = mocks_dir + 'normed_counts_all_' + ID + '.dat'
 
@@ -33,8 +55,7 @@ def main():
         plt.clf()
         plt.figure(1)
 
-        dd_mean_true = np.mean(dd, axis=0)
-        dd_std_true = np.std(dd, axis=0)
+        dd_mean_true = np.std(dd, axis=0)
 
         for i in range(len(Nmock_list)):
 
@@ -42,12 +63,9 @@ def main():
 
             N = Nmock_list[i]
 
-            np.random.shuffle(dd)
-
             dd_new = dd[:N]
 
-            dd_mean = np.mean(dd_new, axis=0)
-            dd_std  = np.std(dd_new, axis=0)
+            dd_std = np.mean(dd_new, axis=0)
 
             dd_std_frac = (dd_std - dd_std_true) / dd_std_true
 
@@ -55,17 +73,16 @@ def main():
 
         plt.legend(loc='upper left', fontsize=10)
         plt.axis([min(bin_cent), max(bin_cent), -0.2, 0.2])
-        # plt.axis([min(bin_cent), max(bin_cent), 0, 0.025])
-        figname='std_' + ID + '.png'
-        plt.xlabel('Bin Center (kpc)', fontsize=14)
-        plt.ylabel(r'$\frac{\sigma_{N}-\sigma_{10000}}{\sigma_{10000}}$', fontsize=18)
+        figname='sigma_' + ID + '.png'
+        plt.xlabel('Bin Center (kpc)', fontsize=10)
+        plt.ylabel(r'$\frac{\overline{\sigma_{N}}-\overline{\sigma_{10000}}}{\overline{\sigma_{10000}}}$', fontsize=18)
+        plt.tight_layout()
         plt.savefig(figname)
 
+        file_list.append(figname)
 
-
-
-
-
+    gif_name = 'sigmas.gif'
+    GIF_MOVIE(file_list, gif_name, removef=True)
 
 
 if __name__ == '__main__':
