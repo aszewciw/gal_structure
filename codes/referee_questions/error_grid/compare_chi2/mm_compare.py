9bcdff9db424ccdaf00f056aec1365fef648dde6
aszewciw <aszewci@gmail.com>
2016-11-16 11:54:51 -0600

    iji

--- codes/referee_questions/error_grid/compare_chi2/mm_compare.py
+++ codes/referee_questions/error_grid/compare_chi2/mm_compare.py
@@ -14,7 +14,7 @@ Make a gif from the files.
 
 def main():
 
-    elements_needed = int(2)
+    elements_needed = int(3)
     args_array      = np.array(sys.argv)
     N_args          = len(args_array)
     assert(N_args == elements_needed)
