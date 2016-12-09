932ae204ff4992e5e77f16d8043afafbdc306d68
aszewciw <aszewci@gmail.com>
2016-12-09 09:57:23 -0600

    iji

--- codes/referee_questions/mcmc_cov_fid_errors/mcmc_mpi/src/errors.c
+++ codes/referee_questions/mcmc_cov_fid_errors/mcmc_mpi/src/errors.c
@@ -50,9 +50,12 @@ double calculate_chi2(POINTING *p, int N_bins, int lower_ind, int upper_ind){
                 sigma_k = p[i].rbin[k].sigma;
 
                 /* add contribution to chi2 from correlation matrix element */
-                chi2_temp = ( ( ( corr_data_j - corr_model_j ) / sigma_j )
-                    * ( ( corr_data_k - corr_model_k ) / sigma_k )
-                    * p[i].invcor_row[j].invcor_col[k] );
+                // chi2_temp = ( ( ( corr_data_j - corr_model_j ) / sigma_j )
+                //     * ( ( corr_data_k - corr_model_k ) / sigma_k )
+                //     * p[i].invcor_row[j].invcor_col[k] );
+
+                chi2_temp = ( (corr_data_j - corr_model_j)
+                    * (corr_data_j - corr_model_j) / (sigma_j * sigma_j) );
 
                 chi2 += chi2_temp;
             }
