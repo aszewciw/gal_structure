8a804e57f9721b9e3b14427da7b88e6615245a33
aszewciw <aszewci@gmail.com>
2016-12-08 20:49:37 -0600

    mcmc run

--- codes/referee_questions/mcmc_cov_fid_errors/mcmc_mpi/run.sh
+++ codes/referee_questions/mcmc_cov_fid_errors/mcmc_mpi/run.sh
@@ -4,10 +4,11 @@ make cleanall
 make
 
 N_procs=20;
+steps=500000;
 filename="../data/mcmc_output/mcmc_result.dat";
 
 rm $filename
 
-time mpirun -n $N_procs ./bin/run_mcmc -f $filename
+time mpirun -n $N_procs ./bin/run_mcmc -f $filename -N_s $steps
 
 # python plot_disk_params.py
