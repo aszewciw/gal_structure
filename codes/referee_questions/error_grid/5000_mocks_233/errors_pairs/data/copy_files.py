6fbf25695063355fb0bce255e0acc162781913ef
aszewciw <aszewci@gmail.com>
2016-12-19 11:56:08 -0600

    jiji

---
+++ codes/referee_questions/error_grid/5000_mocks_233/errors_pairs/data/copy_files.py
@@ -0,0 +1,20 @@
+import os, sys
+
+def main():
+
+    file_dir = '/fs1/szewciw/gal_structure/codes/referee_questions/10000_mocks/errors_pairs/data'
+
+    i = 1
+
+    while i<5001:
+
+        print('Copying folder ' + str(i))
+        folder = file_dir + '/mock_' + str(i)
+        cmd = 'cp -rp ' + folder + ' ./'
+
+        os.system(cmd)
+
+        i = i+1
+
+if __name__ == '__main__':
+    main()
