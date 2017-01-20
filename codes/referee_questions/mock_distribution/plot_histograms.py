0be8e6189045f413e4eb20ddb33a31e90cc78cd4
aszewciw <aszewci@gmail.com>
2017-01-20 10:44:46 -0600

    trying hist

---
+++ codes/referee_questions/mock_distribution/plot_histograms.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+
+    data_dir = './prepare_mocks/data/'
+    comp_file = data_dir + 'all_stars.dat'
+    samp_file = data_dir + 'selected_stars.dat'
+
+    # all_stars = np.genfromtxt(comp_file)
+    disk_samp, Z_samp, R_samp = np.genfromtxt(samp_file, unpack=True)
+
+    thin_ind  = np.where(disk_samp==0)[0]
+    thick_ind = np.where(disk_samp==1)[0]
+
+    Zthinsamp  = Z_samp[thin_ind]
+    Rthinsamp  = R_samp[thin_ind]
+    Zthicksamp = Z_samp[thick_ind]
+    Rthicksamp = R_samp[thick_ind]
+
+    plt.figure(1)
+    n, bins, patches = plt.hist(Zthinsamp, 50, facecolor='green', alpha=0.75)
+    plt.savefig('zthinsamp.png')
+
+if __name__ == '__main__':
+    main()
