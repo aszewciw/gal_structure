24f51848584420f2d35574cc3b553476118e79b3
aszewciw <aszewci@gmail.com>
2017-01-20 11:00:35 -0600

    now plotting full also

--- codes/referee_questions/mock_distribution/plot_histograms.py
+++ codes/referee_questions/mock_distribution/plot_histograms.py
@@ -9,7 +9,6 @@ def main():
     comp_file = data_dir + 'all_stars.dat'
     samp_file = data_dir + 'selected_stars.dat'
 
-    # all_stars = np.genfromtxt(comp_file)
     disk_samp, Z_samp, R_samp = np.genfromtxt(samp_file, unpack=True)
 
     thin_ind  = np.where(disk_samp==0)[0]
@@ -29,6 +28,27 @@ def main():
     n, bins, patches = plt.hist(Rthinsamp, 50, facecolor='green', alpha=0.5)
     n, bins, patches = plt.hist(Rthicksamp, 50, facecolor='red', alpha=0.5)
     plt.savefig('rsamp.png')
+
+
+
+    disk_all, Z_all, R_all = np.genfromtxt(comp_file, unpack=True)
+
+    thin_ind  = np.where(disk_all==0)[0]
+    thick_ind = np.where(disk_all==1)[0]
+
+    Zthinall  = Z_all[thin_ind]
+    Rthinall  = R_all[thin_ind]
+    Zthickall = Z_all[thick_ind]
+    Rthickall = R_all[thick_ind]
+
+    plt.figure(3)
+    n, bins, patches = plt.hist(Zthinall, 50, facecolor='green', alpha=0.5)
+    n, bins, patches = plt.hist(Zthickall, 50, facecolor='red', alpha=0.5)
+    plt.savefig('zall.png')
+    plt.figure(4)
+    n, bins, patches = plt.hist(Rthinall, 50, facecolor='green', alpha=0.5)
+    n, bins, patches = plt.hist(Rthickall, 50, facecolor='red', alpha=0.5)
+    plt.savefig('rall.png')
 
 if __name__ == '__main__':
     main()
