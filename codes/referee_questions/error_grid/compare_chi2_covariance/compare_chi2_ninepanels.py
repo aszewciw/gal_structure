e624fc2d4637702d8c8c6f935ece9f8e8b90c126
aszewciw <aszewci@gmail.com>
2017-02-27 16:21:57 -0600

    idk

--- codes/referee_questions/error_grid/compare_chi2_covariance/compare_chi2_ninepanels.py
+++ codes/referee_questions/error_grid/compare_chi2_covariance/compare_chi2_ninepanels.py
@@ -93,6 +93,8 @@ def plot_chi2_vs_z0thin(plt_num, z0, chi2, truth_index, dict_key):
     if int(plt_num/337)!=1:
         plt.xticks(z0, ' ')
         # plt.xticklabels([])
+    else:
+        plt.xticks(0.193, 0.233, 0.273)
     plt.subplots_adjust(bottom=0.15)
 
 #------------------------------------------------------------------------------#
